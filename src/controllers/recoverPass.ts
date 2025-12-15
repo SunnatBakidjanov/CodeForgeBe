34e091cc43b94a58c6efc88143dd5d745f8348c7
SunnatBakidjanov <sunnatbackidjanov@gmail.com>
2025-12-15 23:39:04 +0500

    fix: дополнена логика в recoverPass
    --recoverPass: добавлена логика антиспама и продления таймера

--- src/controllers/recoverPass.ts
+++ src/controllers/recoverPass.ts
@@ -12,6 +12,7 @@ export const sendEmailRecoverPass = async (req: Request, res: Response) => {
     }
 
     const expiresAt = new Date(Date.now() + 15 * 60 * 1000);
+    const cooldownMs = 60 * 1000;
 
     try {
         const user = await prisma.user.findUnique({ where: { email } });
@@ -21,19 +22,35 @@ export const sendEmailRecoverPass = async (req: Request, res: Response) => {
             return res.status(404).json({ message: 'User not found' });
         }
 
+        const passwordReset = await prisma.passwordReset.findFirst({ where: { email } });
+
+        if (passwordReset) {
+            const now = Date.now();
+            const lastSent = passwordReset.lastSentAt.getTime();
+
+            if (now - lastSent < cooldownMs) {
+                const waitSec = Math.ceil((cooldownMs - (now - lastSent)) / 1000);
+                return res.status(429).json({ message: `Please wait ${waitSec} seconds before requesting a new code.`, waitSec });
+            }
+
+            await prisma.passwordReset.deleteMany({ where: { email } });
+        }
+
+        const token = crypto.randomUUID();
+        const hashToken = crypto.createHash('sha256').update(token).digest('hex');
+
+        await prisma.passwordReset.create({
+            data: {
+                email,
+                token: hashToken,
+                expiresAt,
+            },
+        });
+
         return res.status(200).json({ message: 'Email sent successfully' });
-
-        // const token = crypto.randomUUID();
-        // const hashToken = crypto.createHash('sha256').update(token).digest('hex');
-
-        // await prisma.passwordReset.create({
-        //     data: {
-        //         email,
-        //         token: hashToken,
-        //         expiresAt,
-        //     },
-        // });
     } catch (error) {
         const err = error as Error;
+        Logger.error(`Server Error\n ${err.message}`, 'sendEmailRecoverPass');
+        return res.status(500).json({ message: err.message });
     }
 };
